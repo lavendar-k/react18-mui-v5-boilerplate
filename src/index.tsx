66cfd9b7cb09eacf6dfa685d66e41fad424f27a9
Lavendar Solution <lavendarsolution@gmail.com>
2024-05-13 12:42:36 -0400

    feat: simple note app

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { AuthContextProvider } from "./Provider/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
-root.render(
-  <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
